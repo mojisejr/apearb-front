2d232defd175dbfbba2a64220ad867a3476afe42
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-05-05 23:55:42 +0700

    fix: update round input for winnerpicking

--- components/HoneyPot/WinnerPicked.tsx
+++ components/HoneyPot/WinnerPicked.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { useGetWinner } from "../../blockchain/HoneyPot/Pot.read";
 
 function WinnerPicked({ round }: { round: string }) {
-  const { winner } = useGetWinner(round);
+  const prev = (parseInt(round) - 1).toString();
+  const { winner } = useGetWinner(prev);
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-pepe_black bg-opacity-90">
       <div className="flex justify-center mt-[15%]">
